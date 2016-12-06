c8792f5f534d5aad34a64ba5130f0d14a67c7dbc
Srinath <sv072002@gmail.com>
2016-12-06 18:20:11 -0500

    Interactive Chemistry Justin Lechner Jason Long Hasan Hamdani Srinath Vasudevan

---
+++ app/src/main/java/afhs/tsa/softdev/interactivechemistry/MainActivity.java
@@ -0,0 +1,26 @@
+package afhs.tsa.softdev.interactivechemistry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ALL ELEMENT CODE WILL BE MOVED TO NEW ACTIVITY
+        int protons; //integer value of protons
+
+        int electrons; //integer value of electrons
+        String element;
+
+        /*
+        List of all the elements in the periodic table
+        Used for interactive atom builder
+        Last updated: 12/6/2016, Srinath
+        */
+
+    }
+}
