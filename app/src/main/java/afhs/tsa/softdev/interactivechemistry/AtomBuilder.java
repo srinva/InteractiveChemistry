569f21b6e2d85ee6f34b584a55732e309af4495b
Srinath <sv072002@gmail.com>
2016-12-10 12:53:26 -0500

    Good News: You can drag the TextView Bad News: It doesn't do what I tell it to do Try to fix the message it displays when it enters the "Drop" TextView
    -Srinath

--- app/src/main/java/afhs/tsa/softdev/interactivechemistry/AtomBuilder.java
+++ app/src/main/java/afhs/tsa/softdev/interactivechemistry/AtomBuilder.java
@@ -1,14 +1,28 @@
 package afhs.tsa.softdev.interactivechemistry;
 
+import android.content.ClipData;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.TextView;
 
 public class AtomBuilder extends AppCompatActivity {
 
+
+    TextView drag, drop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atom_builder);
+
+
+        drag = (TextView) findViewById(R.id.drag);
+        drop = (TextView) findViewById(R.id.drop);
+
+        drag.setOnLongClickListener(longClickListener);
+
+
         
         int protons = 0; //integer value of protons
         int electrons = 0; //integer value of electrons
@@ -233,4 +247,48 @@ public class AtomBuilder extends AppCompatActivity {
 
 
     }
+
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData data = ClipData.newPlainText("","");
+            View.DragShadowBuilder myShadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(data, myShadowBuilder, v, 0);
+            return true;
+        }
+    };
+
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+
+            int dragEvent = event.getAction();
+            switch (dragEvent){
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    final View view = (View) event.getLocalState();
+
+                    if (view.getId() == R.id.drag){
+                        drag.setText("Drag has entered area");
+                    }
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+
+                    /*final View view2 = (View) event.getLocalState();
+
+                    if (view2.getId() == R.id.drag){
+                        drag.setText("Drag has exited area");
+                    }*/
+                    break;
+                case DragEvent.ACTION_DROP:
+
+                    /*final View view3 = (View) event.getLocalState();
+
+                    if (view3.getId() == R.id.drag){
+                        drag.setText("Drag has been dropped in area");
+                    }*/
+                    break;
+            }
+            return true;
+        }
+    };
 }
