9c1e5a11cbcf7ebe9dce710813e851a5ea8eec20
JasonLong24 <jasonlongball@gmail.com>
2016-12-11 09:00:55 -0500

    -Button goes to element page on click -I don't think we should add another activity for an element until we are done with all the info for hydrogen
    -Jason-

---
+++ app/src/main/java/afhs/tsa/softdev/interactivechemistry/Hydrogen.java
@@ -0,0 +1,13 @@
+package afhs.tsa.softdev.interactivechemistry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Hydrogen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hydrogen);
+    }
+}
