801aa35ddb0e49633032f305ca2cde8567403089
JasonLong24 <jasonlongball@gmail.com>
2017-01-14 20:27:25 -0500

    --Jason--
    -did some stuff look lol

--- app/src/main/java/afhs/tsa/softdev/interactivechemistry/ElementIndexActivity.java
+++ app/src/main/java/afhs/tsa/softdev/interactivechemistry/ElementIndexActivity.java
@@ -4,73 +4,84 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.app.SearchManager;
+import android.widget.SearchView;
+import android.widget.SearchView.OnQueryTextListener;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class ElementIndexActivity extends AppCompatActivity {
 
+
+    ListView elementindexlist;
+    SearchView searchElement;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_elementindex);
+        elementindexlist = (ListView) findViewById(R.id.elementindexlist);
+        searchElement = (SearchView) findViewById(R.id.searchElements);
 
-        Button hydrogen = (Button) findViewById(R.id.hydrogendiction);
-        hydrogen.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
+        String[] values = new String[] {
+                "Hydrogen",
+                "Helium",
+                "Lithium"
+        };
 
-                Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
-                dictionaryValues.putExtra("protons", 1);
-                startActivity(dictionaryValues);
 
+        final ArrayAdapter<String>adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, values);
+        elementindexlist.setAdapter(adapter);
+
+        searchElement.setOnQueryTextListener(new OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                return false;
             }
-        });
 
-        Button helium = (Button) findViewById(R.id.heliumdiction);
-        helium.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
-                dictionaryValues.putExtra("protons", 2);
-                startActivity(dictionaryValues);
-
-            }
-        });
-
-        Button lithium = (Button) findViewById(R.id.lithiumdiction);
-        lithium.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
-                dictionaryValues.putExtra("protons", 3);
-                startActivity(dictionaryValues);
-
-            }
-        });
-
-        Button beryllium = (Button) findViewById(R.id.berylliumdiction);
-        beryllium.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
-                dictionaryValues.putExtra("protons", 4);
-                startActivity(dictionaryValues);
-
-            }
-        });
-
-        Button boron = (Button) findViewById(R.id.borondiction);
-        boron.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
-                dictionaryValues.putExtra("protons", 5);
-                startActivity(dictionaryValues);
-
+            @Override
+            public boolean onQueryTextChange(String text) {
+                adapter.getFilter().filter(text);
+                return false;
             }
         });
 
 
+
+        elementindexlist.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                int itemposition = position;
+                String value  = (String) elementindexlist.getItemAtPosition(position);
+                Toast.makeText(ElementIndexActivity.this, ""+value, Toast.LENGTH_SHORT).show();
+
+                if (value == "Hydrogen") {
+                    Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
+                    dictionaryValues.putExtra("protons", 1);
+                    startActivity(dictionaryValues);
+
+                }
+
+                if (value == "Helium") {
+                    Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
+                    dictionaryValues.putExtra("protons", 2);
+                    startActivity(dictionaryValues);
+
+                }
+
+                if (value == "Lithium") {
+                    Intent dictionaryValues = new Intent(ElementIndexActivity.this, ElementInformation.class);
+                    dictionaryValues.putExtra("protons", 3);
+                    startActivity(dictionaryValues);
+
+                }
+            }
+        });
 
 
     }
 }
+
